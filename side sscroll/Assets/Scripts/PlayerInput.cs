bab3b8471060e4af052bb0acdd4184a0002318cb
bloominninja <bloominninja@gmail.com>
2015-02-07 13:59:40 -0800

    How to open
    Open project with unity load bust scene

---
+++ side sscroll/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PlayerPhysics))]
+public class PlayerInput : MonoBehaviour 
+{
+	//Player Handling
+		//Public Values
+			public float speed=8;
+			public float acceleration=12;
+			public float gravity=20;
+			public float jumpHeight=12;
+			public float jumps=1;
+		//Private Values
+			private float currentSpeed;
+			private float targetSpeed;
+			private Vector2 amountToMove;
+			private float RJumps;
+			private PlayerPhysics playerPhysics;
+	
+
+	void Start () {
+
+		playerPhysics=GetComponent<PlayerPhysics>();
+
+	}
+	
+	
+	void Update () 
+	{
+		targetSpeed=Input.GetAxisRaw("Horizontal")*speed;
+		currentSpeed =IncrementTowards(currentSpeed,targetSpeed,acceleration);
+		if(playerPhysics.grounded)
+		{
+			amountToMove.y=0;
+			RJumps=jumps;
+		}	
+		if(Input.GetButtonDown ("Jump")&&RJumps>0)
+			{
+				amountToMove.y=jumpHeight;
+				RJumps-=1;
+		}
+
+		amountToMove.x=currentSpeed;
+		amountToMove.y-=gravity*Time.deltaTime;
+		playerPhysics.Move (amountToMove*Time.deltaTime);
+	}
+	
+	private float IncrementTowards(float n, float target, float a)
+	{
+		if(n==target)
+		{
+			return n;
+		}
+		else
+		{
+			float dir=Mathf.Sign (target-n);
+			n+= a*Time.deltaTime*dir;
+			
+			if(dir==Mathf.Sign (target-n))
+			{
+				return n;
+			}
+			else
+			{
+				return target;
+			}
+		}
+	}
+}
---
+++ side sscroll/Assets/Scripts/PlayerPhysics.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider))]
+
+public class PlayerPhysics : MonoBehaviour 
+{
+	public LayerMask collisionMask;
+
+	private BoxCollider collider;
+	private Vector3 s;
+	private Vector3 c;
+	//NEVER CHANGE BELOW LINE OR UNITY COMPILE FUNNY
+	private float skin =0.005f;
+
+	[HideInInspector]
+	public bool grounded;
+	[HideInInspector]
+	public bool wall;
+
+	Ray ray;
+	RaycastHit hit;
+	void Start()
+	{
+		collider=GetComponent<BoxCollider>();
+		s=collider.size;
+		c=collider.center;
+	}
+public void Move(Vector2 moveAmount)
+	{
+		float deltaY=moveAmount.y;
+		float deltaX=moveAmount.x;
+		Vector2 p=transform.position;
+		grounded=false;
+		for(int i=0;i<3;i++)
+		{
+			float dir=Mathf.Sign(deltaY);
+			float x =(p.x + c.x - s.x/2) +s.x/2 * i;
+			float y =p.y + c.y + s.y/2*dir;
+
+			ray=new Ray(new Vector2(x,y), new Vector2(0,dir));
+Debug.DrawRay(ray.origin,ray.direction);
+			if( Physics.Raycast(ray,out hit,Mathf.Abs(deltaY),collisionMask))
+			{
+				float dst = Vector3.Distance (ray.origin, hit.point);
+				if(dst>skin)
+				{
+					deltaY = dst*dir + skin;
+				}
+				else
+				{
+					deltaY=0;
+
+				}
+				grounded=true;
+				break;
+			}
+		}
+		for(int i=0;i<5;i++)
+		{
+			float dir=Mathf.Sign(deltaX);
+			float y =(p.y+ c.y - s.y/4) +s.y/4 * (i-1);
+			float x =p.x + c.x + s.x/4*dir;
+			
+			ray=new Ray(new Vector2(x,y), new Vector2(dir,0));
+			Debug.DrawRay(ray.origin,ray.direction);
+		}
+
+
+
+		Vector2 finalTransform=new Vector2(deltaX,deltaY);
+		transform.Translate (finalTransform);
+	}
+}
