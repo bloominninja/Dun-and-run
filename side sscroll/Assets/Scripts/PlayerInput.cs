f7dbc3ebaf75e3eff624b4082d073a2be6ca4692
luei <luei333@gmail.com>
2015-02-10 03:39:18 -0800

    Start of multiple characters
    Got 2 separate characters in, one controlled with the keyboard and one
    with joy 1.

--- side sscroll/Assets/Scripts/PlayerInput.cs
+++ side sscroll/Assets/Scripts/PlayerInput.cs
@@ -2,114 +2,109 @@
 using System.Collections;
 
 [RequireComponent(typeof(PlayerPhysics))]
-public class PlayerInput : MonoBehaviour 
+public class PlayerInput : MonoBehaviour
 {
 	//Player Handling
-		//Public Values
-			public float speed=8;
-			public float acceleration=12;
-			public float gravity=20;
-			public float jumpHeight=12;
-			public float jumps=1;
-		//Private Values
-			private float currentSpeed;
-			private float targetSpeed;
-			private Vector2 amountToMove;
-			private float RJumps;
-			private PlayerPhysics playerPhysics;
-			private bool right;
-			private bool wallJump;
+	//Public Values
+	public float speed = 8;
+	public float acceleration = 12;
+	public float gravity = 20;
+	public float jumpHeight = 12;
+	public float jumps = 1;
+	//Private Values
+	private float currentSpeed;
+	private float targetSpeed;
+	private Vector2 amountToMove;
+	private float RJumps;
+	private PlayerPhysics playerPhysics;
+	private bool right;
+	private bool wallJump;
 
-	
+	//input string variables, use any of the strings in the Input Manager
+	//can be changed on the fly to rebind keys and set up control devices
+	public string H = "Horizontal";
+	public string V = "Vertical";
+	public string Jump = "Jump";
+	public string Basic = "Fire1";
+	public string Special;
+	public string Item1;
+	public string Item2;
+	public string Grab1;
+	public string Grab2;
 
-	void Start () {
+	void Start ()
+	{
 
-		playerPhysics=GetComponent<PlayerPhysics>();
+		playerPhysics = GetComponent<PlayerPhysics> ();
 		//right=true;
+		
 	}
 	
 	
-	void Update () 
+	void Update ()
 	{
-		targetSpeed=Input.GetAxisRaw("Horizontal")*speed;
-		currentSpeed =IncrementTowards(currentSpeed,targetSpeed,acceleration);
+		targetSpeed = Input.GetAxisRaw (H) * speed;
+		currentSpeed = IncrementTowards (currentSpeed, targetSpeed, acceleration);
 
-		if(playerPhysics.grounded)
-		{
-			amountToMove.y=0;
-			RJumps=jumps;
-			wallJump=false;
+		if (playerPhysics.grounded) {
+			amountToMove.y = 0;
+			RJumps = jumps;
+			wallJump = false;
 		}	
-		if(playerPhysics.stopMove)
-		{
-			if (currentSpeed>0)
-			{
-				right=true;
-				Debug.Log("right");
+		if (playerPhysics.stopMove) {
+			if (currentSpeed > 0) {
+				right = true;
+				Debug.Log ("right");
+			} else if (currentSpeed < 0) {
+				right = false;
+				Debug.Log ("left");
 			}
-			else if (currentSpeed<0)
-			{
-				right=false;
-				Debug.Log("left");
+			if (!playerPhysics.grounded) {
+				wallJump = true;
 			}
-			if(!playerPhysics.grounded)
-			{
-				wallJump=true;
-			}
-			targetSpeed=0;
-			currentSpeed=0;
+			targetSpeed = 0;
+			currentSpeed = 0;
 
 		}
-		if(Input.GetButtonDown ("Jump"))
-			{
+		if (Input.GetButtonDown (Jump)) {
 
-			if(wallJump)
-				{
-				if(right)
-				{
-					currentSpeed=-18;
-					right=false;
+			if (wallJump) {
+				if (right) {
+					currentSpeed = -18;
+					right = false;
+				} else {
+					currentSpeed = 18;
+					right = true;
 				}
-				else
-				{
-					currentSpeed=18;
-					right=true;
-				}
-				amountToMove.y=jumpHeight;
+				amountToMove.y = jumpHeight;
 
-				}
-			if((RJumps>0)&&wallJump==false)
-				{
-				amountToMove.y=jumpHeight;
-				RJumps-=1;
-				}
-			wallJump=false;
+			}
+			if ((RJumps > 0) && wallJump == false) {
+				amountToMove.y = jumpHeight;
+				RJumps -= 1;
+			}
+			wallJump = false;
 		}
 
-		amountToMove.x=currentSpeed;
-		amountToMove.y-=gravity*Time.deltaTime;
-		playerPhysics.Move (amountToMove*Time.deltaTime);
+		amountToMove.x = currentSpeed;
+		amountToMove.y -= gravity * Time.deltaTime;
+		playerPhysics.Move (amountToMove * Time.deltaTime);
 	}
 	
-	private float IncrementTowards(float n, float target, float a)
+	private float IncrementTowards (float n, float target, float a)
 	{
-		if(n==target)
-		{
+		if (n == target) {
 			return n;
-		}
-		else
-		{
-			float dir=Mathf.Sign (target-n);
-			n+= a*Time.deltaTime*dir;
+		} else {
+			float dir = Mathf.Sign (target - n);
+			n += a * Time.deltaTime * dir;
 			
-			if(dir==Mathf.Sign (target-n))
-			{
+			if (dir == Mathf.Sign (target - n)) {
 				return n;
-			}
-			else
-			{
+			} else {
 				return target;
 			}
 		}
 	}
+	
 }
--- side sscroll/Assets/Scripts/PlayerPhysics.cs
+++ side sscroll/Assets/Scripts/PlayerPhysics.cs
@@ -3,111 +3,103 @@ using System.Collections;
 
 [RequireComponent(typeof(BoxCollider))]
 
-public class PlayerPhysics : MonoBehaviour 
+public class PlayerPhysics : MonoBehaviour
 {
 	public LayerMask collisionMask;
 
-	private BoxCollider collider;
+	private BoxCollider box;
 	private Vector3 s;
 	private Vector3 c;
 	//NEVER CHANGE BELOW LINE OR UNITY COMPILE FUNNY
-	private float skin =0.005f;
+	private float skin = 0.005f;
 
 	[HideInInspector]
-	public bool grounded;
+	public bool
+		grounded;
 
 	[HideInInspector]
-	public bool stopMove;
+	public bool
+		stopMove;
 
 	[HideInInspector]
-	public bool touchWall;
+	public bool
+		touchWall;
 
 	Ray ray;
 	RaycastHit hit;
-	void Start()
+	void Start ()
 	{
-		collider=GetComponent<BoxCollider>();
-		s=collider.size;
-		c=collider.center;
+		box = GetComponent<BoxCollider> ();
+		s = box.size;
+		c = box.center;
 	}
-public void Move(Vector2 moveAmount)
+	public void Move (Vector2 moveAmount)
 	{
 
-		float deltaY=moveAmount.y;
-		float deltaX=moveAmount.x;
-		Vector2 p=transform.position;
-		grounded=false;
+		float deltaY = moveAmount.y;
+		float deltaX = moveAmount.x;
+		Vector2 p = transform.position;
+		grounded = false;
 
 
-	//Check Bot/Top detection
-		for(int i=0;i<3;i++)
-		{
-			float dir=Mathf.Sign(deltaY);
-			float x =(p.x + c.x - s.x/2) +s.x/2 * i;
-			float y =p.y + c.y + s.y/2*dir;
+		//Check Bot/Top detection
+		for (int i=0; i<3; i++) {
+			float dir = Mathf.Sign (deltaY);
+			float x = (p.x + c.x - s.x / 2) + s.x / 2 * i;
+			float y = p.y + c.y + s.y / 2 * dir;
 
-			ray=new Ray(new Vector2(x,y), new Vector2(0,dir));
-			Debug.DrawRay(ray.origin,ray.direction);
-			if( Physics.Raycast(ray,out hit,Mathf.Abs(deltaY)+skin,collisionMask))
-			{
+			ray = new Ray (new Vector2 (x, y), new Vector2 (0, dir));
+			Debug.DrawRay (ray.origin, ray.direction);
+			if (Physics.Raycast (ray, out hit, Mathf.Abs (deltaY) + skin, collisionMask)) {
 				float dst = Vector3.Distance (ray.origin, hit.point);
-				if(dst>skin)
-				{
-					deltaY = dst*dir - skin*dir;
-				}
-				else
-				{
-					deltaY=0;
+				if (dst > skin) {
+					deltaY = dst * dir - skin * dir;
+				} else {
+					deltaY = 0;
 
 				}
-				grounded=true;
+				grounded = true;
 				break;
 			}
 		}
 	
-	//Check Left/Right
-		stopMove=false;
-		touchWall=false;
-		for(int i=0;i<3;i++)
-		{
-			float dir=Mathf.Sign(deltaX);
-			float x =p.x + c.x + s.x/2 *dir;
-			float y =p.y + c.y - s.y/2 + s.y/2*i;
+		//Check Left/Right
+		stopMove = false;
+		touchWall = false;
+		for (int i=0; i<3; i++) {
+			float dir = Mathf.Sign (deltaX);
+			float x = p.x + c.x + s.x / 2 * dir;
+			float y = p.y + c.y - s.y / 2 + s.y / 2 * i;
 			
-			ray=new Ray(new Vector2(x,y), new Vector2(dir,0));
-			Debug.DrawRay(ray.origin,ray.direction);
-			if( Physics.Raycast(ray,out hit,Mathf.Abs(deltaX)+skin,collisionMask))
-			{
+			ray = new Ray (new Vector2 (x, y), new Vector2 (dir, 0));
+			Debug.DrawRay (ray.origin, ray.direction);
+			if (Physics.Raycast (ray, out hit, Mathf.Abs (deltaX) + skin, collisionMask)) {
 				float dst = Vector3.Distance (ray.origin, hit.point);
-				if(dst>skin)
-				{
-					deltaX = dst*dir - skin*dir;
+				if (dst > skin) {
+					deltaX = dst * dir - skin * dir;
 
-				}
-				else
-				{
-					deltaX=0;
+				} else {
+					deltaX = 0;
 
 					
 				}
-				stopMove=true;
-				touchWall=true;
+				stopMove = true;
+				touchWall = true;
 
 				//grounded=true;
 				break;
 			}
 		}
 
-		Vector3 playerDir =new Vector3(deltaX,deltaY);
-		Vector3 o = new Vector3 (p.x + c.x + s.x/2 *Mathf.Sign (deltaX),p.y + c.y + s.y/2*Mathf.Sign (deltaY));
-		ray= new Ray(o,playerDir.normalized);
-		if( Physics.Raycast(ray,Mathf.Sqrt((deltaX*deltaX)+(deltaY*deltaY)),collisionMask))
-		{
-			grounded=true;
-			deltaY=0;
+		Vector3 playerDir = new Vector3 (deltaX, deltaY);
+		Vector3 o = new Vector3 (p.x + c.x + s.x / 2 * Mathf.Sign (deltaX), p.y + c.y + s.y / 2 * Mathf.Sign (deltaY));
+		ray = new Ray (o, playerDir.normalized);
+		if (Physics.Raycast (ray, Mathf.Sqrt ((deltaX * deltaX) + (deltaY * deltaY)), collisionMask)) {
+			grounded = true;
+			deltaY = 0;
 		}
 
-		Vector2 finalTransform=new Vector2(deltaX,deltaY);
+		Vector2 finalTransform = new Vector2 (deltaX, deltaY);
 		transform.Translate (finalTransform);
 	}
 }
